7c912ff274ab80289ca0d6d6648bed1c51e548ac
Ririn <ririn.nfa@gmail.com>
2020-11-02 17:46:28 +0700

    Tugas 2 Javascript
    Menampilkan isi dari tipe-tipe data

---
+++ tugas2.js
@@ -0,0 +1,14 @@
+var dataNumber = 173;
+var dataString = "Ini adalah data String";
+var dataBoolean = true;
+var dataNull = null;
+var dataUndifined;
+var dataArray = [1, 7, 3];
+
+console.log("ini adalah data number dengan nilai " + dataNumber,
+            "\n" + dataString, 
+            "\n" +  "ini adalah data boolean dengan nilai " + dataBoolean,
+            "\n" +  "ini adalah data bernilai " + dataNull,
+            "\n" +  "ini adalah data " + dataUndifined,
+            "\n" +  "ini adalah array dengan data " + dataArray
+            );
